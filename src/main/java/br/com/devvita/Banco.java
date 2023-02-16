b62096c3107cd734e856c790a8328aa6fe1e3e90
Rômulo <silva3265@gmail.com>
2023-02-16 14:52:28 -0300

    Exercício – Implementando POO

---
+++ src/main/java/br/com/devvita/Banco.java
@@ -0,0 +1,11 @@
+package br.com.devvita;
+
+public interface Banco {
+
+
+	public void sacar(double valor);
+	public void depositar(double valor);
+	public void transferir(Conta outraConta, double valor);
+
+}
+ 
---
+++ src/main/java/br/com/devvita/Conta.java
@@ -0,0 +1,91 @@
+package br.com.devvita;
+
+public class Conta implements Banco {
+
+	/* Atributos */
+	protected String nome;
+	protected int agencia;
+	protected String numero;
+	protected double saldo;
+	protected double tranferencia;
+
+	/* Construtor com Parametros */
+	public Conta(String nome, int agencia, String numero, double saldo) {
+		this.nome = nome;
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = saldo;
+
+	}
+
+	/* Getters e Setters */
+	public String getNome() {
+		return nome;
+	}
+
+	public double getTranferencia() {
+		return tranferencia;
+	}
+
+	public void setTranferencia(double tranferencia) {
+		this.tranferencia = tranferencia;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	/* Metodo Sacar */
+	public void sacar(double valorSaque) {
+		saldo -= valorSaque;
+
+	}
+
+	/* Metodo Depositar */
+	public void depositar(double valorDeposito) {
+		saldo += valorDeposito;
+
+	}
+
+	/* Metodo Tranferir */
+	public void transferir(Conta outraConta, double valor) {
+		if (valor <= saldo) {
+			this.sacar(valor);
+			outraConta.depositar(valor);
+		}
+	}
+
+	public void imprimirDemonstrativo() {
+		System.out.println();
+		System.out.println("Nome: " + getNome());
+		System.out.println("Agencia: " + getAgencia());
+		System.out.println("Numero da Conta: " + getNumero());
+		System.out.println("Saldo: " + getSaldo());
+
+	}
+
+}
